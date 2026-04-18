3c68073eab67274e2c598fd763bacf1c6c6a7bdf
ayoub <ayoubjomni@proton.me>
2026-04-18 18:08:33 +0100

    working prototype

--- image-score.py
+++ image-score.py
@@ -1,6 +1,7 @@
 import cv2
-
+import os
 from skimage.metrics import structural_similarity as ssim
+import sys
 
 # Function to calculate SSIM
 def calculate_ssim(imageA, imageB):
@@ -14,11 +15,30 @@ def calculate_ssim(imageA, imageB):
     score, _ = ssim(grayA, grayB, full=True)
     return score
 
+if __name__=="__main__":
 
-# Load sample images
-imageA = cv2.imread('/home/aybi/SI/indexation/projet/image-search-by-similarity/imageA.jpg')
-imageB = cv2.imread('/home/aybi/SI/indexation/projet/image-search-by-similarity/imageB.jpg')
 
-# Calculate SSIM
-ssim_score = calculate_ssim(imageA, imageB)
-print(f"SSIM score: {ssim_score}")
+    if len(sys.argv) != 3:
+        print('no arguments or too many arguments are given')
+        exit(1)
+    else:
+        image = sys.argv[1]
+        dire = sys.argv[2]
+        
+    if not(os.path.isfile(image)):
+        print(f'{image} does not exists')
+    elif '.png' not in image and '.jpg' not in image:
+        print(f'{image} is not an image file')
+    elif not os.path.isdir(dire):
+        print(f"{dire} is not a directory")
+    else:
+
+        for (roots , dires , file) in os.walk(dire):
+            print(roots)
+            for f in file:
+                if '.jpg' in f or '.png' in f:
+
+                    print(f'{f} : {calculate_ssim(cv2.imread(image),cv2.imread(roots+"/"+f))}')
+    
+    
+    
