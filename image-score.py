ff5795dcc3e9d023d7d99fac6d71bbc96e8a860c
ayoub <ayoubjomni@proton.me>
2026-04-18 11:39:23 +0100

    test

--- image-score.py
+++ image-score.py
@@ -1,1 +1,24 @@
-print('test')
+import cv2
+
+from skimage.metrics import structural_similarity as ssim
+
+# Function to calculate SSIM
+def calculate_ssim(imageA, imageB):
+    # Convert images to grayscale
+    grayA = cv2.cvtColor(imageA, cv2.COLOR_BGR2GRAY)
+    grayB = cv2.cvtColor(imageB, cv2.COLOR_BGR2GRAY)
+    grayA = cv2.resize(grayA, (780, 540), interpolation=cv2.INTER_LINEAR)
+    grayB = cv2.resize(grayB, (780, 540), interpolation=cv2.INTER_LINEAR)
+    
+    # Compute SSIM between the two images
+    score, _ = ssim(grayA, grayB, full=True)
+    return score
+
+
+# Load sample images
+imageA = cv2.imread('/home/aybi/SI/indexation/projet/image-search-by-similarity/imageA.jpg')
+imageB = cv2.imread('/home/aybi/SI/indexation/projet/image-search-by-similarity/imageB.jpg')
+
+# Calculate SSIM
+ssim_score = calculate_ssim(imageA, imageB)
+print(f"SSIM score: {ssim_score}")
