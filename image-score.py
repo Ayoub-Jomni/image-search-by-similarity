1b8d5d664c5a74d2390334e8d53027ebfcbd4be0
ayoub <ayoubjomni@proton.me>
2026-04-19 11:07:06 +0100

    added visuals for outputed images

--- image-score.py
+++ image-score.py
@@ -1,18 +1,21 @@
 import cv2
 import os
 from skimage.metrics import structural_similarity as ssim
+from matplotlib import pyplot as plt
 import sys
+
+
+def sortFunc(e):
+    return e["score"]
 
 # Function to calculate SSIM
 def calculate_ssim(imageA, imageB):
     # Convert images to grayscale
-    grayA = cv2.cvtColor(imageA, cv2.COLOR_BGR2GRAY)
     grayB = cv2.cvtColor(imageB, cv2.COLOR_BGR2GRAY)
-    grayA = cv2.resize(grayA, (780, 540), interpolation=cv2.INTER_LINEAR)
     grayB = cv2.resize(grayB, (780, 540), interpolation=cv2.INTER_LINEAR)
     
     # Compute SSIM between the two images
-    score, _ = ssim(grayA, grayB, full=True)
+    score, _ = ssim(imageA, grayB, full=True)
     return score
 
 if __name__=="__main__":
@@ -32,13 +35,39 @@ if __name__=="__main__":
     elif not os.path.isdir(dire):
         print(f"{dire} is not a directory")
     else:
+        image = cv2.imread(image)    
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        image = cv2.resize(image, (780, 540), interpolation=cv2.INTER_LINEAR)
+
+        imageScore = []
+
 
         for (roots , dires , file) in os.walk(dire):
             print(roots)
             for f in file:
                 if '.jpg' in f or '.png' in f:
 
-                    print(f'{f} : {calculate_ssim(cv2.imread(image),cv2.imread(roots+"/"+f))}')
+                    imageScore.append({"score":calculate_ssim(image,cv2.imread(roots+'/'+f)),"file":roots+'/'+f})
+        
+        imageScore.sort(key = sortFunc,reverse=True)            
+        
+        rows = len(imageScore)//4
+        print('rows:',rows)
+        print(len(imageScore))
+        fig = plt.figure(figsize=(10, 7))
+
+        for i in range(0,rows):
+            print(i)
+            for j in range(4):
+                plt.subplot(rows, 4, i*4+j+1)  
+                plt.imshow(cv2.cvtColor(cv2.imread(imageScore[i*4+j]['file']), cv2.COLOR_BGR2RGB))  
+                plt.axis('off')  
+                plt.title(f"score: {imageScore[i+j]['score']:.3f}")
+
+        plt.show()
+    
+        
+                                     
     
     
     
