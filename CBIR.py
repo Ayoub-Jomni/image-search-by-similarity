97ff6af7903a8c16ab6f96c4709715941871b836
ayoub <ayoubjomni@proton.me>
2026-04-24 19:48:00 +0100

    final push

--- CBIR.py
+++ CBIR.py
@@ -1,9 +1,7 @@
 import cv2
-from skimage.metrics import structural_similarity as ssim
 from matplotlib import pyplot as plt
 import os
 import sys
-
 import torch
 import torch.nn as nn
 from torchvision import models, transforms
@@ -32,15 +30,6 @@ def cosine_similarity(featureA, featureB):
 def sortFunc(e):
     return e["score"]
 
-# # Function to calculate SSIM
-# def calculate_ssim(imageA, imageB):
-#     # Convert images to grayscale
-#     grayB = cv2.cvtColor(imageB, cv2.COLOR_BGR2GRAY)
-#     grayB = cv2.resize(grayB, (780, 540), interpolation=cv2.INTER_LINEAR)
-#
-#     # Compute SSIM between the two images
-#     score, diff = ssim(imageA, grayB, full=True)
-#     return score
 
 if __name__=="__main__":
 
@@ -67,7 +56,6 @@ if __name__=="__main__":
         for (roots , dires , file) in os.walk(dire):
             for f in file:
                 if '.jpg' in f or '.png' in f:
-
                     imageB = preprocess_image(roots+'/'+f)
                     
                     # Extract features
@@ -78,20 +66,21 @@ if __name__=="__main__":
                     # Compare using cosine similarity
                     similarity = cosine_similarity(featuresA, featuresB)
                     
-                    imageScore.append({"score": similarity,"file":roots+'/'+f})
+                    imageScore.append({"score": similarity,"file":roots+'/'+f,'image':imageB})
+                        
          
         imageScore.sort(key = sortFunc,reverse=True)            
         
-        rows = (len(imageScore)//4)
-        fig = plt.figure(figsize=(10, 7))
-        if rows > 5:
-            rows = 5
-        for i in range(0,rows):
-            for j in range(4):
-                plt.subplot(rows, 4, i*4+j+1)  
-                plt.imshow(cv2.cvtColor(cv2.imread(imageScore[i*4+j]['file']), cv2.COLOR_BGR2RGB))  
-                plt.axis('off')  
-                plt.title(f"score: {imageScore[i*4+j]['score'].item():.3f}")
+        fig = plt.figure(figsize=(20, 7))
+        for i in range(0,1):
+            for j in range(6):
+                try:
+                    plt.subplot(1, 6, i*4+j+1)  
+                    plt.imshow(cv2.cvtColor(cv2.imread(imageScore[i*4+j]['file']), cv2.COLOR_BGR2RGB))  
+                    plt.axis('off')  
+                    plt.title(f"score: {imageScore[i*4+j]['score'].item():.4f}")
+                except:
+                    break
 
         plt.show()
     
